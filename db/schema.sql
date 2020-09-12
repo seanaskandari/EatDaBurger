f772e2bb0fb63cc5795a68aba102fcdca2305c80
Sean Askandari <seanaskandari@gmail.com>
2020-09-11 21:07:15 -0500

    server,db,json config folders setup

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+CREATE TABLE IF NOT EXISTS burgers (
+id INT AUTO_INCREMENT PRIMARY KEY
+, burger_name VARCHAR(30)
+, devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false)
+, ("Spicy Chicken Burger", false)
+, ("Veggie Burger", false)
+, ("BBQ Burger", false);
+SELECT * FROM burgers;
